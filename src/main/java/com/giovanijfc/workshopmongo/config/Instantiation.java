51dece844f180678f4f1457dc27ef2d5df187d8c
giovanijfc <giovanijfc@gmail.com>
2019-03-18 17:22:57 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/giovanijfc/workshopmongo/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.giovanijfc.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.giovanijfc.workshopmongo.domain.User;
+import com.giovanijfc.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		userRepo.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		userRepo.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
