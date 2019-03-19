fa776414a251a8ba9b8b3acc522171878e3eed8f
giovanijfc <giovanijfc@gmail.com>
2019-03-19 14:47:50 -0300

    Consulta simples com @query

--- src/main/java/com/giovanijfc/workshopmongo/services/PostService.java
+++ src/main/java/com/giovanijfc/workshopmongo/services/PostService.java
@@ -21,7 +21,7 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text) {
-		return postRepo.findByTitleContainingIgnoreCase(text);
+		return postRepo.searchTitle(text);
 	}
 }
 	
